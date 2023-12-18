f278ea743c57f9961141f12733ec25bc76565cb6
Emmanuel Matiko <emmanuel.kichuki@gmail.com>
2023-12-18 14:33:59 +0300

    Add files via upload

---
+++ MPC/MPC.js
@@ -0,0 +1,15 @@
+document.getElementById("MPCForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    calculatePayment();
+});
+
+function calculatePayment() {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var interest = parseFloat(document.getElementById("interest").value) / 100 / 12;
+    var term = parseFloat(document.getElementById("term").value) * 12;
+
+    var monthlyPayment = (principal * interest * Math.pow(1 + interest, term)) / (Math.pow(1 + interest, term) - 1);
+    monthlyPayment = monthlyPayment.toFixed(2);
+
+    alert("Your estimated monthly MPC payment is: Tsh " + monthlyPayment);
+}
